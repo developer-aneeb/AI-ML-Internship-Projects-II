ddf7c6858ab02308f8e1a837950b94261e9fcd64
developer-aneeb <dev.aneeb.rehman@gmail.com>
2026-06-12 03:26:31 +0500

    Context_Aware_Chatbot _Using_LangChain_RAG Task

--- Context_Aware_Chatbot/extract.ps1
+++
@@ -1,8 +0,0 @@
-$nb = Get-Content -Raw "Context_Aware_RAG_Chatbot.ipynb" | ConvertFrom-Json
-$output = @()
-foreach ($cell in $nb.cells) {
-    if ($cell.cell_type -eq "markdown") {
-        $output += $cell.source -join ""
-    }
-}
-$output | Out-File -FilePath "markdown.txt" -Encoding utf8
